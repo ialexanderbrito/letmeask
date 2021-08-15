365b209d6662bb71a42bf8a519dc397092965de0
ialexanderbrito <xanderzinho26@gmail.com>
2021-08-14 23:26:51 -0300

    feat: liked question

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 
 import { database } from '../services/firebase';
+import { useAuth } from './useAuth';
 
 type QuestionType = {
   id: string;
@@ -11,6 +12,8 @@ type QuestionType = {
   content: string;
   isHighlighted: boolean;
   isAnswered: boolean;
+  likeCount: number;
+  likeId: string | undefined;
 };
 
 type FirebaseQuestions = Record<
@@ -23,10 +26,12 @@ type FirebaseQuestions = Record<
     content: string;
     isHighlighted: boolean;
     isAnswered: boolean;
+    likes: Record<string, { authorId: string }>;
   }
 >;
 
 export function useRoom(roomId: string) {
+  const { user } = useAuth();
   const [questions, setQuestions] = useState<QuestionType[]>([]);
   const [title, setTitle] = useState('');
 
@@ -43,13 +48,21 @@ export function useRoom(roomId: string) {
           author: value.author,
           isHighlighted: value.isHighlighted,
           isAnswered: value.isAnswered,
+          likeCount: Object.values(value.likes ?? {}).length,
+          likeId: Object.entries(value.likes ?? {}).find(
+            ([key, like]) => like.authorId === user?.id
+          )?.[0],
         })
       );
 
       setTitle(databaseRoom.title);
       setQuestions(parsedQuestions);
+
+      return () => {
+        roomRef.off('value');
+      };
     });
-  }, []);
+  }, [roomId, user?.id]);
 
   return { questions, title };
 }
